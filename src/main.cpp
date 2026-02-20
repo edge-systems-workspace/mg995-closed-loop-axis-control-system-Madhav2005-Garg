0d43c3ab5be745f9796cf3100160967b7f4dbd41
Madhav2005-Garg <madhavgarg545@gmail.com>
2026-02-20 21:34:06 +0530

    add reverse sweep loop from 180 to 0 degrees

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,14 @@ void setup() {
 
 // ... setup code same as above ...
 
+// ... setup code same as above ...
+
 void loop() {
     for (int i = 0; i <= 180; i++) {
+        myServo.write(i);
+        delay(15);
+    }
+    for (int i = 180; i >= 0; i--) {
         myServo.write(i);
         delay(15);
     }
